a22e366009b656cb915992988a2b4a09176d390d
wouter <wouter@debruijn.dev>
2026-03-14 12:27:13 +0100

    chore: formatting + TODO

--- src/branded/opaque-id.ts
+++ src/branded/opaque-id.ts
@@ -1,4 +1,6 @@
 import { Schema } from 'effect'
 
+//TODO: https://x.com/kitlangton/status/2032652945410367920/photo/1
+
 /* eslint-disable no-control-regex */
 export const opaqueId = Schema.String.check(Schema.isMinLength(1), Schema.isMaxLength(255), Schema.isPattern(/^[^\u0000:]+$/u))
