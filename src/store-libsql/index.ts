b642c5ea5647eb7383c50bf82400480e8f4fa7fd
wouter <wouter@debruijn.dev>
2026-04-13 21:11:29 +0200

    feat: remove store from client

--- src/store-libsql/index.ts
+++
@@ -1,5 +0,0 @@
-import { Layer } from 'effect'
-
-import { roomTimelineRepositoryLibSQLLive } from './repository/room-timeline-repository'
-
-export const layerLibSQL = Layer.mergeAll(roomTimelineRepositoryLibSQLLive)
